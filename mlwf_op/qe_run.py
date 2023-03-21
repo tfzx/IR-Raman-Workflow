f662fdcd1323b4fb94b771c0982701fd28c5771a
tfzx <2362595573@qq.com>
2023-03-22 00:09:50 +0800

    print the outputs to screen

--- mlwf_op/qe_run.py
+++ mlwf_op/qe_run.py
@@ -5,11 +5,17 @@ from dflow.utils import (
     set_directory,
     run_command
 )
-import shutil, subprocess, sys, shlex
+import shutil
 
 class RunMLWFQe(RunMLWF):
     def __init__(self) -> None:
         super().__init__()
+
+    def run(self, if_print = True, *args, **kwargs):
+        ret, out, err = run_command(*args, **kwargs)
+        if if_print:
+            print(out)
+        # TODO: Save log here.
 
     def init_cmd(self, commands: Dict[str, str]):
         self.pw_cmd = commands.get("pw", "pw.x")
@@ -18,17 +24,18 @@ class RunMLWFQe(RunMLWF):
         self.wannier90_pp_cmd = commands.get("wannier90_pp", "wannier90.x")
     
     def run_one_frame(self, backward_dir_name: str, backward_list: List[str]) -> Path:
-        run_command(" ".join([self.pw_cmd, "-input", "scf.in"]))
+        self.run(" ".join([self.pw_cmd, "-input", "scf.in"]))
         if Path("nscf.in").exists():
-            run_command(" ".join([self.pw_cmd, "-input", "nscf.in"]))
-        run_command(" ".join([self.wannier90_pp_cmd, "-pp", self.name]))
-        run_command(" ".join(["mpirun -n 2 pw2wannier90.x"]), input=Path(f"{self.name}.pw2wan").read_text())
-        run_command(" ".join([self.wannier_cmd, self.name]))
-        run_command("rm -rf out")
+            self.run(" ".join([self.pw_cmd, "-input", "nscf.in"]))
+        self.run(" ".join([self.wannier90_pp_cmd, "-pp", self.name]))
+        self.run(" ".join(["mpirun -n 2 pw2wannier90.x"]), input=Path(f"{self.name}.pw2wan").read_text())
+        self.run(" ".join([self.wannier_cmd, self.name]))
+        self.run(False, "rm -rf out")
         backward_dir = Path(backward_dir_name)
         backward_dir.mkdir()
         for f in backward_list:
             for p in Path(".").glob(f):
+                print(p.name)
                 if p.is_file():
                     shutil.copy(p, backward_dir)
                 else:
