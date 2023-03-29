c2b25708616bbee50711f2ce6c6f9dbec0092ef8
tfzx <2362595573@qq.com>
2023-03-29 17:06:15 +0800

    change the default params

--- mlwf_op/qe_prepare.py
+++ mlwf_op/qe_prepare.py
@@ -12,32 +12,41 @@ from dflow.python import (
     PythonOPTemplate,
 )
 from mlwf_op.prepare_input_op import Prepare
-from mlwf_op.inputs import QePwInputs, QePw2Wannier90Inputs, Wannier90Inputs, complete_qe, complete_wannier90, complete_pw2wan
+from mlwf_op.inputs import QeParamsConfs, QeParams, Wannier90Inputs, complete_qe, complete_wannier90, complete_pw2wan
+from mlwf_op.utils import complete_by_default
 
 class PrepareQe(Prepare):
+    DEFAULT_PARAMS = {
+        "control": {
+            "restart_mode"  : "from_scratch",
+            "prefix"        : "h2o",
+            "outdir"        : "out",
+            "pseudo_dir"    : "../pseudo",
+        }
+    }
+
     def __init__(self):
         super().__init__()
 
     def init_inputs(self, input_setting: Dict[str, Union[str, dict]], confs: dpdata.System):
         self.name = input_setting["name"]
         self.run_nscf = input_setting["dft_params"]["cal_type"] == "scf+nscf"
-        input_scf, kpoints_scf = complete_qe(
-            input_setting["dft_params"]["qe_params"], 
-            "scf", 
-            input_setting["dft_params"]["k_grid"], 
-            confs
-        )
-        self.scf_writer = QePwInputs(input_scf, kpoints_scf, input_setting["dft_params"]["atomic_species"], confs)
+
+        k_grid = input_setting["dft_params"]["k_grid"]
+        qe_params = complete_by_default(input_setting["dft_params"]["qe_params"], params_default = self.DEFAULT_PARAMS, if_copy = True)
+
+        input_scf, kpoints_scf = complete_qe(qe_params, "scf", k_grid, confs)
+        self.scf_writer = QeParamsConfs(input_scf, kpoints_scf, input_setting["dft_params"]["atomic_species"], confs)
         if self.run_nscf:
-            input_nscf, kpoints_nscf = complete_qe(
-                input_setting["dft_params"]["qe_params"], 
-                "nscf", 
-                input_setting["dft_params"]["k_grid"], 
-                confs
+            input_nscf, kpoints_nscf = complete_qe(qe_params, "nscf", k_grid, confs)
+            self.nscf_writer = QeParamsConfs(input_nscf, kpoints_nscf, input_setting["dft_params"]["atomic_species"], confs)
+        input_pw2wan = complete_pw2wan(
+            input_setting["dft_params"]["pw2wan_params"], 
+            self.name, 
+            input_scf["control"]["prefix"],
+            input_scf["control"]["outdir"]
             )
-            self.nscf_writer = QePwInputs(input_nscf, kpoints_nscf, input_setting["dft_params"]["atomic_species"], confs)
-        input_pw2wan = complete_pw2wan(input_setting["dft_params"]["pw2wan_params"], self.name)
-        self.pw2wan_writer = QePw2Wannier90Inputs(input_pw2wan)
+        self.pw2wan_writer = QeParams(input_pw2wan)
         wan_params, proj, kpoints = complete_wannier90(
             input_setting["wannier90_params"]["wan_params"], 
             input_setting["wannier90_params"]["projections"],
