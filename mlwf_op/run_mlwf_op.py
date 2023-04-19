f2c35915cf237f29611db38fcbbaf55994807c19
tfzx <2362595573@qq.com>
2023-04-19 15:27:11 +0800

    use abs path for log file

--- mlwf_op/run_mlwf_op.py
+++ mlwf_op/run_mlwf_op.py
@@ -43,6 +43,7 @@ class RunMLWF(OP, abc.ABC):
     ) -> OPIO:
         task_path: Path = op_in["task_path"]
         self.name: str = op_in["input_setting"]["name"]
+        self.input_setting = op_in["input_setting"]
         task_setting: dict = op_in["task_setting"]
         self.backward_list: List[str] = task_setting["backward_list"]
         self.backward_dir_name: str = task_setting["backward_dir_name"]
@@ -50,7 +51,7 @@ class RunMLWF(OP, abc.ABC):
         start_f, end_f = op_in["frames"]
 
         confs_path = [Path(f"conf.{f:06d}") for f in range(start_f, end_f)]
-        self.log_path = Path("run.log")
+        self.log_path = Path("run.log").absolute()
 
         self.init_cmd(commands)
         backward = self._exec_all(task_path, confs_path)
@@ -79,8 +80,13 @@ class RunMLWF(OP, abc.ABC):
         shutil.copy(self.log_path, backward_dir)
 
     def run(self, *args, **kwargs):
-        kwargs["prinf_oe"] = True
+        kwargs["print_oe"] = False
+        kwargs["raise_error"] = False
         ret, out, err = run_command(*args, **kwargs)
+        if ret != 0:
+            print(out)
+            print(err)
+            assert ret == 0
         # Save log here.
         with self.log_path.open(mode = "a") as fp:
             fp.write(out)
