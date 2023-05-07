4b8cd44eb7194fcf01f6ffd027b64041c621aacc
tfzx <2362595573@qq.com>
2023-05-07 22:51:25 +0800

    modify dp_setting

--- spectra_flow/dp/dp_train.py
+++ spectra_flow/dp/dp_train.py
@@ -1,6 +1,6 @@
 from typing import Dict, List, Tuple, Union
 from pathlib import Path
-import dpdata, numpy as np, json
+import dpdata, numpy as np, json, abc
 from dflow.python import (
     OP, 
     OPIO, 
@@ -14,7 +14,7 @@ from dflow.utils import (
 )
 from spectra_flow.utils import filter_confs, read_conf
 
-class DPTrain(OP):
+class DPTrain(OP, abc.ABC):
     def __init__(self) -> None:
         super().__init__()
         self.dp_type = "dipole" # "dipole" or "polar"
@@ -28,7 +28,7 @@ class DPTrain(OP):
         return OPIOSign({
             "confs": Artifact(Path),
             "conf_fmt": BigParameter(dict),
-            "train_inputs": BigParameter(Dict),
+            "dp_setting": BigParameter(Dict),
             "label": Artifact(Path),
         })
 
@@ -45,13 +45,19 @@ class DPTrain(OP):
     ) -> OPIO:
         confs = read_conf(op_in["confs"], op_in["conf_fmt"])
         label = np.loadtxt(op_in["label"], dtype = float)
-        train_inputs = op_in["train_inputs"]
+        dp_setting: dict = op_in["dp_setting"]
+        train_inputs: dict = dp_setting["train_inputs"]
+        label = self.preprocess(label, dp_setting)
         train_dir = self.prepare_train(train_inputs, confs, label)
         model = self.run_train(train_dir)
         return OPIO({
             "frozen_model": model
         })
-    
+
+    @abc.abstractmethod
+    def preprocess(self, label: np.ndarray, dp_setting: dict) -> np.ndarray:
+        return label
+
     def prepare_train(self, train_inputs: Dict, confs: dpdata.System, label: np.ndarray, set_size: int = 5000):
         data_dir = Path("data")
         train_dir = Path("train")
@@ -84,8 +90,15 @@ class DWannTrain(DPTrain):
     def __init__(self) -> None:
         super().__init__()
         self.dp_type = "dipole"
+    
+    def preprocess(self, label: np.ndarray, dp_setting: dict) -> np.ndarray:
+        amplif = dp_setting.get("amplif", 1.0)
+        return label * amplif
 
 class DPolarTrain(DPTrain):
     def __init__(self) -> None:
         super().__init__()
         self.dp_type = "polar"
+    
+    def preprocess(self, label: np.ndarray, dp_setting: dict) -> np.ndarray:
+        return label
