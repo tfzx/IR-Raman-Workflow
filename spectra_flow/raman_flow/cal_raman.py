b10d2b8960afbeeca5fe409efc13e5d6ed31df1c
tfzx <2362595573@qq.com>
2023-05-10 16:29:53 +0800

    add cal_raman

---
+++ spectra_flow/raman_flow/cal_raman.py
@@ -0,0 +1,52 @@
+import numpy as np
+from spectra_flow.utils import (
+    calculate_corr,
+    apply_gussian_filter,
+    FT
+)
+
+def calculate_corr_polar(polar: np.ndarray, window: int):
+    polar_iso = np.mean(polar.diagonal(offset = 0, axis1 = 1, axis2 = 2), axis = 1)
+
+    diag = np.zeros((polar_iso.shape[0], 3, 3), dtype = float)
+    diag[:, 0, 0] = polar_iso
+    diag[:, 1, 1] = polar_iso
+    diag[:, 2, 2] = polar_iso
+    polar_aniso = polar - diag
+
+    polar_iso -= np.mean(polar_iso, axis = 0, keepdims = True)
+    polar_aniso -= np.mean(polar_aniso, axis = 0, keepdims = True)
+
+    corr_iso = calculate_corr(polar_iso, polar_iso, window)
+    polar_aniso = polar_aniso.reshape(-1, 9)
+    corr_aniso = calculate_corr(polar_aniso, polar_aniso, window)
+    corr_aniso *= 2 / 15
+    corr_aniso -= np.mean(corr_aniso, axis = 0)
+    return corr_iso, corr_aniso
+
+def calculate_raman(corr: np.ndarray, width: int, dt_ps: float, temperature: float):
+    nmax = corr.shape[0] - 1
+    if nmax % 2 != 0:
+        nmax -= 1
+        corr = corr[:-1]
+    tmax = nmax * dt_ps        # ps
+    # dom = 2. * np.pi / tmax
+    print('nmax =', nmax)
+    print('dt   =', dt_ps)
+    print('tmax =', tmax)
+    print("width = ", width)
+    width = width * tmax / 100.0 * 3.0
+    C = apply_gussian_filter(corr, width)
+    CHAT = FT(dt_ps, C)
+    print(CHAT[0])
+    print(np.mean(C))
+    cc = 2.99792458e8;      # m/s
+    kB = 1.38064852*1.0e-23 # J/K
+    h = 6.62607015e-34      # J*s
+    h_bar = h / (2 * np.pi)
+    beta = 1.0 / (kB * temperature); 
+    d_omega = 1e10 / (tmax * cc)
+    omega = np.arange(CHAT.shape[0]) * d_omega
+    freq = 2 * np.pi / tmax * 1e12
+    CHAT = CHAT * 1e4 * (1 - np.exp(-beta * h_bar * freq * np.arange(CHAT.shape[0])))
+    return np.stack([omega, CHAT], axis = 1)
---
+++ spectra_flow/raman_flow/raman_op.py
@@ -0,0 +1,56 @@
+from typing import Dict
+from pathlib import Path
+import dpdata, numpy as np
+from dflow.python import (
+    OP, 
+    OPIO, 
+    Artifact, 
+    OPIOSign, 
+    BigParameter,
+)
+from spectra_flow.raman_flow.cal_raman import calculate_corr_polar, calculate_raman
+
+class CalIR(OP):
+    def __init__(self) -> None:
+        super().__init__()
+    
+    @classmethod
+    def get_input_sign(cls):
+        return OPIOSign({
+            "global": BigParameter(Dict),
+            "total_polar": Artifact(Path)
+        })
+
+    @classmethod
+    def get_output_sign(cls):
+        return OPIOSign({
+            "raman_iso": Artifact(Path),
+            "raman_aniso": Artifact(Path)
+        })
+
+    @OP.exec_sign_check
+    def execute(
+            self,
+            op_in: OPIO,
+    ) -> OPIO:
+        global_config = op_in["global"]
+        dt = global_config["dt"]
+        width = global_config["width"]
+        temperature = global_config["temperature"]
+        window = global_config["window"]
+
+        total_polar = np.load(op_in["total_polar"])
+        
+        corr_iso, corr_aniso = calculate_corr_polar(total_polar, window)
+        raman_iso = calculate_raman(corr_iso, width = width, dt_ps = dt, temperature = temperature)
+        raman_aniso = calculate_raman(corr_aniso, width = width, dt_ps = dt, temperature = temperature)
+
+        raman_iso_path = Path("raman_iso.raw")
+        raman_aniso_path = Path("raman_iso.raw")
+        np.savetxt(raman_iso_path, raman_iso, fmt = "%15.8f")
+        np.savetxt(raman_aniso_path, raman_aniso, fmt = "%15.8f")
+        return OPIO({
+            "raman_iso": raman_iso_path,
+            "raman_aniso": raman_aniso_path
+        })
+    
