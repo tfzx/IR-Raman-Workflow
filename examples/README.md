7f69e1d1a9eeb192df9fdb4ecd526f7cb1cc9388
tfzx <2362595573@qq.com>
2023-05-17 00:36:09 +0800

    add examples of ir_flow

---
+++ examples/README.md
@@ -0,0 +1,10 @@
+# account_config
+
+File `account_config` (only support bohrium now):
+```json
+{
+    "username": "your username",
+    "password": "******",
+    "project_id": 000000
+}
+```
---
+++ examples/ir_workflow/example.1/run.py
@@ -0,0 +1,30 @@
+import time, matplotlib.pyplot as plt, numpy as np
+from dflow import Workflow, download_artifact
+from spectra_flow.ir_flow.ir import build_ir
+from spectra_flow.utils import bohrium_login, load_json
+
+if __name__ == "__main__":
+    bohrium_login(load_json("../../account.json"))
+
+    # 4 steps: dipole, train, predict, cal_ir
+    run_config = {
+        "start_steps": "predict",
+        "end_steps": "cal_ir"
+    }
+    ir_step = build_ir(load_json("global.json"), load_json("../machine.json"), run_config)
+
+    wf = Workflow("ir-workflow")
+    wf.add(ir_step)
+    wf.submit()
+    while wf.query_status() in ["Pending", "Running"]:
+        time.sleep(1)
+    assert(wf.query_status() == "Succeeded")
+    ir_path = download_artifact(wf.query_step("water")[0].outputs.artifacts["ir"], path = "back")[0]
+    ir = np.loadtxt(ir_path)
+    plt.plot(ir[:, 0], ir[:, 1] / 1000, label = r'$D_2O$, calculated', scalex = 1.5, scaley= 2.2)
+    plt.xlim((0, 4000.))
+    plt.ylim((0, 15))
+    plt.xlabel(r'Wavenumber($\rm cm^{-1}$)', fontdict = {'size': 12})
+    plt.ylabel(r'$n(\omega)\alpha(\omega) (10^3 cm^{-1})$', fontdict = {'size': 12})
+    plt.legend()
+    plt.show()
