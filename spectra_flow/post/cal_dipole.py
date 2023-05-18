620d6989adcde20416be93dd7c2a15207e14fb05
tfzx <2362595573@qq.com>
2023-05-18 16:49:04 +0800

    move cal_wc to cal_dipole.py

--- spectra_flow/post/cal_dipole.py
+++ spectra_flow/post/cal_dipole.py
@@ -1,5 +1,5 @@
-import numpy as np
-from spectra_flow.utils import box_shift
+import numpy as np, dpdata
+from spectra_flow.utils import k_nearest, box_shift
 
 def _fix_coords(coords_sel: np.ndarray, coords_oth: np.ndarray, cells: np.ndarray, r_bond: float, mask_sel: np.ndarray = None):
     coords_sel = coords_sel[..., np.newaxis, :]
@@ -33,11 +33,38 @@ def fix_coords(coords_sel: np.ndarray, coords_oth: np.ndarray, cells: np.ndarray
     np.apply_along_axis(fix, axis = -1, arr = c)
     return c.reshape(coords_sel.shape[0], -1, 3)[:, num_sel:-1, :]
 
+def cal_wc_h2o(wfc: np.ndarray, coords_O: np.ndarray, cells: np.ndarray) -> np.ndarray:
+    """
+        Calculate the wannier centroids for system of H2O.
+
+        Parameters
+        -------------
+            wfc      (..., num_wann, 3): the coordinates of the wannier function centers. 
+
+            coords_O (..., num_O, 3): the coordinates of the O atoms. It should be num_wann = 4 * num_O.
+            
+            cells    (..., 3, 3)
+
+        Return
+        -------------
+            wannier centroids (..., num_O, 3): the wannier centroid relative to each O atoms.
+    """
+    idx = k_nearest(coords_O, wfc, cells, k = 4)
+    wfc = np.take_along_axis(wfc[..., np.newaxis, :, :], idx[..., np.newaxis], axis = -2)
+    return np.mean(box_shift(wfc - coords_O[..., np.newaxis, :], cells[..., np.newaxis, np.newaxis, :, :]), axis = -2)
+
 def calculate_dipole_h2o(coords_sel: np.ndarray, coords_oth: np.ndarray, cells: np.ndarray, wannier: np.ndarray, r_bond = 1.2) -> np.ndarray:
     coords_oth = fix_coords(coords_sel, coords_oth, cells, r_bond)
     total_dipole = np.sum(coords_oth, axis = 1) - np.sum(coords_sel, axis = 1) * 2. - np.sum(wannier, axis = 1) * 8.
     vol = np.linalg.det(cells)[..., np.newaxis]
     return total_dipole / np.sqrt(vol) * np.sqrt(0.52917721067)
+
+def cal_wc(confs: dpdata.System, wfc: np.ndarray) -> np.ndarray:
+        return cal_wc_h2o(
+            wfc.reshape(confs.get_nframes(), -1, 3), 
+            confs["coords"][:, confs["atom_types"] == 0], 
+            confs["cells"]
+        ).reshape(confs.get_nframes(), -1)
 
 if __name__ == '__main__':
     type_O, type_H = 0, 1
