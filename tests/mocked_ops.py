15237a8f3c40e727be95919b489ea3957f5ee896
tfzx <2362595573@qq.com>
2023-06-06 19:49:27 +0800

    fix: update the interface of mocked_ops

--- tests/mocked_ops.py
+++ tests/mocked_ops.py
@@ -1,23 +1,27 @@
+from types import ModuleType
 from typing import Dict, List, Tuple, Union
 from spectra_flow.mlwf.mlwf_ops import Prepare, RunMLWF
 from pathlib import Path
 import dpdata, os
 
 class MockedPrepare(Prepare):
-    def init_inputs(self, mlwf_setting: Dict[str, Union[str, dict]], confs: dpdata.System):
+    def init_inputs(self, 
+                    mlwf_setting: Dict[str, Union[str, dict]], 
+                    confs: dpdata.System,
+                    wc_python: ModuleType = None) -> Dict[str, Union[str, dict]]:
         self.task_path = []
         self.conf_path = []
         return super().init_inputs(mlwf_setting, confs)
     
-    def write_one_frame(self, frame: int):
+    def prep_one_frame(self, frame: int):
         cwd = Path(os.getcwd())
         self.conf_path.append(cwd.name)
         self.task_path.append(cwd.parent.name)
-        return super().write_one_frame(frame)
+        return super().prep_one_frame(frame)
 
 class MockedRunMLWF(RunMLWF):
-    def init_cmd(self, commands: Dict[str, str]):
-        return super().init_cmd(commands)
+    def init_cmd(self, mlwf_setting: Dict[str, Union[str, dict]], commands: Dict[str, str]):
+        return super().init_cmd(mlwf_setting, commands)
 
-    def run_one_frame(self) -> Path:
-        return super().run_one_frame()
+    def run_one_frame(self, backward_dir: Path):
+        return super().run_one_frame(backward_dir)
