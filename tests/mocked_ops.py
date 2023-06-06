341e10a50a9d7459983f871ccc2020dfbf4a77d7
tfzx <2362595573@qq.com>
2023-06-07 03:51:10 +0800

    add test collect

--- tests/mocked_ops.py
+++ tests/mocked_ops.py
@@ -1,6 +1,8 @@
 from types import ModuleType
-from typing import Dict, List, Tuple, Union
-from spectra_flow.mlwf.mlwf_ops import Prepare, RunMLWF
+from typing import Dict, List, Optional, Tuple, Union
+
+import numpy as np
+from spectra_flow.mlwf.mlwf_ops import CollectWFC, Prepare, RunMLWF
 from pathlib import Path
 import dpdata, os
 
@@ -25,3 +27,20 @@ class MockedRunMLWF(RunMLWF):
 
     def run_one_frame(self, backward_dir: Path):
         return super().run_one_frame(backward_dir)
+    
+class MockedCollectWFC(CollectWFC):
+    def __init__(self, success_list: Optional[List[bool]] = None) -> None:
+        self.success_list = success_list
+        super().__init__()
+
+    def init_params(self, mlwf_setting: dict, conf_sys: dpdata.System, example_file: Path):
+        self.confs = conf_sys
+
+    def get_one_frame(self) -> Dict[str, np.ndarray]:
+        if self.success_list is not None:
+            if not self.success_list.pop(0):
+                raise RuntimeError("Test: raise error.")
+        return {
+            "test1": np.zeros((10, 3)),
+            "test2": np.ones((9, 3))
+        }
--- tests/test_collect.py
+++ tests/test_collect.py
@@ -1,16 +1,95 @@
-from spectra_flow.mlwf.mlwf_ops import CollectWFC
+from mocked_ops import MockedCollectWFC
+from spectra_flow.utils import read_conf
+import unittest, shutil, os
 from pathlib import Path
 import numpy as np
 
-def test_collect():
-    collect_op = CollectWFC()
-    op_in = {
-        "name": "water",
-        "confs": Path("./data"),
-        "backward": [Path("./back")]
-    }
-    op_out = collect_op.execute(op_in)
-    print(np.loadtxt(op_out["wannier_function_centers"], dtype = float))
+class TestCollect(unittest.TestCase):
+    def setUp(self) -> None:
+        super().setUp()
+        self.confs = Path("tests/data.qe/test.003/data").absolute()
+        self.conf_fmt = {
+            "fmt": "deepmd/raw",
+            "type_map": ["O", "H"]
+        }
+        self.work_dir = Path("tests/tmp")
+        self.work_dir.mkdir()
+        self.last_cwd = os.getcwd()
+        os.chdir(self.work_dir)
+        self.backs = [Path(f"task.{i:06d}") for i in range(2)]
+        for p in self.backs:
+            p.mkdir()
+    
+    def tearDown(self) -> None:
+        os.chdir(self.last_cwd)
+        if self.work_dir.is_dir():
+            shutil.rmtree(self.work_dir)
+        return super().tearDown()
 
-if __name__ == "__main__":
-    test_collect()
+
+    def test_collect_all_success(self):
+        collect_op = MockedCollectWFC()
+        ori_confs = read_conf(self.confs, self.conf_fmt)
+        op_in = {
+            "mlwf_setting": {},
+            "confs": self.confs,
+            "conf_fmt": self.conf_fmt,
+            "backward": self.backs
+        }
+        op_out = collect_op.execute(op_in)
+        wfc = op_out["wannier_function_centers"]
+        final_confs = op_out["final_confs"]
+        final_conf_fmt = op_out["final_conf_fmt"]
+        final_confs = read_conf(final_confs, final_conf_fmt)
+        self.assertEqual(final_confs.get_nframes(), 2)
+        self.assert_(np.allclose(ori_confs["coords"], final_confs["coords"]))
+        self.assert_(op_out["failed_confs"].exists())
+        wfc1 = np.loadtxt(wfc["test1"], dtype = float).reshape(-1, 10, 3)
+        wfc2 = np.loadtxt(wfc["test2"], dtype = float).reshape(-1, 9, 3)
+        self.assert_(np.allclose(wfc1, np.zeros((2, 10, 3))))
+        self.assert_(np.allclose(wfc2, np.ones((2, 9, 3))))
+
+    def test_collect_one_fail(self):
+        collect_op = MockedCollectWFC(success_list = [True, False])
+        ori_confs = read_conf(self.confs, self.conf_fmt)
+        op_in = {
+            "mlwf_setting": {},
+            "confs": self.confs,
+            "conf_fmt": {
+                "fmt": "deepmd/raw",
+                "type_map": ["O", "H"]
+            },
+            "backward": self.backs
+        }
+        op_out = collect_op.execute(op_in)
+        wfc = op_out["wannier_function_centers"]
+
+        final_confs = op_out["final_confs"]
+        final_conf_fmt = op_out["final_conf_fmt"]
+        final_confs = read_conf(final_confs, final_conf_fmt)
+        self.assertEqual(final_confs.get_nframes(), 1)
+        self.assert_(np.allclose(ori_confs[0]["coords"], final_confs["coords"]))
+
+        failed_confs = op_out["failed_confs"]
+        self.assert_(failed_confs.exists())
+        failed_confs = read_conf(failed_confs, {"fmt": "deepmd/npy"})
+        self.assertEqual(failed_confs.get_nframes(), 1)
+        self.assert_(np.allclose(ori_confs[1]["coords"], failed_confs["coords"]))
+
+        wfc1 = np.loadtxt(wfc["test1"], dtype = float).reshape(-1, 10, 3)
+        wfc2 = np.loadtxt(wfc["test2"], dtype = float).reshape(-1, 9, 3)
+        self.assert_(np.allclose(wfc1, np.zeros((1, 10, 3))))
+        self.assert_(np.allclose(wfc2, np.ones((1, 9, 3))))
+        
+    def test_collect_all_fail(self):
+        collect_op = MockedCollectWFC(success_list = [False, False])
+        op_in = {
+            "mlwf_setting": {},
+            "confs": self.confs,
+            "conf_fmt": {
+                "fmt": "deepmd/raw",
+                "type_map": ["O", "H"]
+            },
+            "backward": self.backs
+        }
+        self.assertRaises(AssertionError, collect_op.execute, op_in)
