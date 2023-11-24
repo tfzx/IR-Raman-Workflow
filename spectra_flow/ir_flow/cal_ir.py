b884a6119c0fd272147e29a777e2f24434163841
tfzx <2362595573@qq.com>
2023-11-25 02:28:45 +0800

    add lorenz filter

--- spectra_flow/ir_flow/cal_ir.py
+++ spectra_flow/ir_flow/cal_ir.py
@@ -3,27 +3,36 @@ import numpy as np
 from spectra_flow.utils import (
     calculate_corr,
     apply_gussian_filter,
+    apply_lorenz_filter,
     FT
 )
 
 def calculate_corr_vdipole(dipole: np.ndarray, dt_ps: float, window: int):
-    v_dipole = (dipole[2:] - dipole[:-2]) / (2 * dt_ps) # type: ignore
+    v_dipole = (dipole[1:] - dipole[:-1]) / dt_ps # type: ignore
     v_dipole -= np.mean(v_dipole, axis = 0, keepdims = True)
     corr = np.sum(calculate_corr(v_dipole, v_dipole, window), axis = -1)
     return corr
 
-def calculate_ir(corr: np.ndarray, width: float, dt_ps: float, temperature: float, M: Optional[int] = None):
+def calculate_ir(corr: np.ndarray, width: float, dt_ps: float, temperature: float, 
+                 M: Optional[int] = None, filter_type: str = "gaussian"):
     nmax = corr.shape[0] - 1
     if nmax % 2 != 0:
         nmax -= 1
         corr = corr[:-1]
     tmax = nmax * dt_ps
-    print('nmax      =', nmax)
-    print('dt   (ps) =', dt_ps)
-    print('tmax (ps) =', tmax)
-    print("width     = ", width)
-    width = width * tmax / 100.0 * 3
-    C = apply_gussian_filter(corr, width)
+    filter_type = filter_type.lower().strip()
+    print("nmax         =", nmax)
+    print("dt   (ps)    =", dt_ps)
+    print("tmax (ps)    =", tmax)
+    print("Filter type  =", filter_type)
+    print("Smooth width =", width)
+    if filter_type == "gaussian":
+        width = width * tmax / 100.0 * 3
+        C = apply_gussian_filter(corr, width)
+    elif filter_type == "lorenz":
+        C = apply_lorenz_filter(corr, width, dt_ps)
+    else:
+        raise NotImplementedError(f"Unknown filter type: {filter_type}!")
     freq_ps, CHAT = FT(dt_ps, C, M)
     d_omega, CHAT = _change_unit(freq_ps, CHAT, temperature)
     return np.stack([np.arange(CHAT.shape[0]) * d_omega, CHAT], axis = 1)
